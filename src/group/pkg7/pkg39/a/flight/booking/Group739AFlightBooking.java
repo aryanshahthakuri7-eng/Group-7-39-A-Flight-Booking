f39f82ef46362a57fb94cc909ff92b0c7b61449e
aryanshahthakuri7-eng <aryanshahthakuri7@gmail.com>
2026-05-05 12:39:21 +0545

    updated code

---
+++ src/group/pkg7/pkg39/a/flight/booking/Group739AFlightBooking.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package group.pkg7.pkg39.a.flight.booking;
+
+/**
+ *
+ * @author Lenovo LOQ
+ */
+public class Group739AFlightBooking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
