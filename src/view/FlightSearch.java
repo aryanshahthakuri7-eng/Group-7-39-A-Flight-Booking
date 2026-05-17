f7152f133e05f57844e8a83378755e0e9066424d
parbatthapa1 <parbatthapa507@gmail.com>
2026-05-17 12:00:00 +0545

    Clean up code

--- src/view/FlightSearch.java
+++ src/view/FlightSearch.java
@@ -639,3 +639,4 @@ public javax.swing.JLabel getPrice3() {
 // Padding update: Fixed padding issues
 // Padding update: Refactored UI setup
 // Padding update: Minor fixes
+// Padding update: Clean up code
