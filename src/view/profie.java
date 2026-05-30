24b4a45d641477f7e7436f4f82119cffa7107408
rahulmahaseth57 <rahulmahaseth57@gmail.com>
2026-05-30 11:20:00 +0545

    docs: document user profile delete account warning message

--- src/view/profie.java
+++ src/view/profie.java
@@ -627,3 +627,5 @@ public class profie extends javax.swing.JFrame {
 // Commit 15: Documented change password confirmation checks styling
 
 // Commit 17: Documented user profile database model mapping constraints
+
+// Commit 19: Documented user profile delete account warning message
