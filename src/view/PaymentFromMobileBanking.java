727900725cc03101fe3d6ce9bbcc34bd87a38286
siddharthsharma7717 <siddharthsharma7717@gmail.com>
2026-06-02 14:00:00 +0545

    docs: document bank accounts list dropdown selection bindings

--- src/view/PaymentFromMobileBanking.java
+++ src/view/PaymentFromMobileBanking.java
@@ -909,3 +909,5 @@ public class PaymentFromMobileBanking extends javax.swing.JFrame {
 // Commit 20: Documented confirm payment button validation triggers
 
 // Commit 22: Documented IME Pay logo resolution painting properties
+
+// Commit 24: Documented bank accounts list dropdown selection bindings
