72eb79ca2a48f8c7ee97a32d67e00e4629852c39
siddharthsharma7717 <siddharthsharma7717@gmail.com>
2026-06-06 11:20:00 +0545

    docs: document card secure lock symbol icon rendering properties

--- src/view/PaymentFromCard.java
+++ src/view/PaymentFromCard.java
@@ -999,3 +999,5 @@ public class PaymentFromCard extends javax.swing.JFrame {
 // Commit 25: Documented card expiry check invalid input indicators
 
 // Commit 27: Documented credit card background color gradients specs
+
+// Commit 29: Documented card secure lock symbol icon rendering properties
