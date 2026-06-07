db189978a7d66cbf8045602dcb567acc4bef69cd
aryanshahthakuri7 <aryanshahthakuri7@gmail.com>
2026-06-07 08:20:23 +0545

    feat: Implement UserDao and UserDaoImpl for database authentication queries

---
+++ src/dao/UserDao.java
@@ -0,0 +1,9 @@
+package dao;
+
+import model.User;
+
+public interface UserDao {
+    boolean createUser(User user);
+    User getUserByEmail(String email);
+    boolean updatePassword(String email, String newPassword);
+}
