dda5c7ac3bae19316f013779532b5efadcbbb5a6
aryanshahthakuri7 <aryanshahthakuri7@gmail.com>
2026-06-11 15:45:00 +0545

    docs: document login controller redirection to home verification

--- src/view/login.java
+++ src/view/login.java
@@ -509,3 +509,5 @@ public class login extends javax.swing.JFrame {
 // Commit 16: Documented login page show password check state listener
 
 // Commit 19: Documented login panel custom logo icon bounds
+
+// Commit 22: Documented login controller redirection to home verification
